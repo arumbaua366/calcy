5aa29c0c89b4980301dfea2b124e6dda96d16385
arumbaua366 <arumbaua366@gmail.com>
2023-10-14 19:14:39 -0700

    add actions on button clicks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "*", "+", "-", "."];
+
+  const updateCalc = (value) => {
+    setCalc(calc + value);
+  };
+
   const createDigits = () => {
     const digits = [];
 
@@ -13,19 +24,20 @@ function App() {
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>(0)</span> 0
+          {result ? <span>(0)</span> : ""}
+          {calc || "0"}
         </div>
         <div className="operators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
           <button>DEL</button>
         </div>
         <div className="digits">
           {createDigits()}
-          <button>0</button>
-          <button>.</button>
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
           <button>=</button>
         </div>
       </div>
