e875fdd8b5b279b9da9c213cd895fc1691b179ff
arumbaua366 <arumbaua366@gmail.com>
2023-10-14 19:41:56 -0700

    update updateCalc function to prevent consecutive operators from being typed out and to calculate result

--- src/App.js
+++ src/App.js
@@ -7,14 +7,30 @@ function App() {
   const ops = ["/", "*", "+", "-", "."];
 
   const updateCalc = (value) => {
+    if (
+      // prevents being able to type consecutive operators
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+
     setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
   };
 
   const createDigits = () => {
     const digits = [];
 
     for (let i = 1; i < 10; i++) {
-      digits.push(<button key={i}>{i}</button>);
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
+      );
     }
 
     return digits;
@@ -24,7 +40,7 @@ function App() {
     <div className="App">
       <div className="calculator">
         <div className="display">
-          {result ? <span>(0)</span> : ""}
+          {result ? <span>({result})</span> : ""}
           {calc || "0"}
         </div>
         <div className="operators">
