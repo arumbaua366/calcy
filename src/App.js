cf62165fabc1720b6f11dc47b922e3a06d218b69
arumbaua366 <arumbaua366@gmail.com>
2023-10-14 18:22:36 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+        <div className="operators">
+          <button>/</button>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
